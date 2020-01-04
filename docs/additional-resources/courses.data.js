560099356196343115d2d483c06feee38f3a29a7
bravo-kernel <bravo-kernel@users.noreply.github.com>
2020-01-04 11:48:52 +0100

    feat: use PesterDataTable for /courses page

---
+++ docs/additional-resources/courses.data.js
@@ -0,0 +1,15 @@
+// ----------------------------------------------------------------------------
+// Please respect chronological (date) order when adding new entries.
+// ----------------------------------------------------------------------------
+export const courses = [
+  {
+    title: "Testing PowerShell with Pester",
+    author: "Ashley McGlone, Adam Bertram",
+    url: "https://mva.microsoft.com/en-US/training-courses/17650?l=mg8oBM9vD_8811787177"
+  },
+  {
+    title: "PSKoans - A simple, fun, and interactive way to learn the PowerShell language through Pester unit testing",
+    author: "Joel Sallow and a Community",
+    url: "https://github.com/vexx32/PSKoans"
+  }
+];
