f5ee88de5a47ff83554a5ea6f03981e3d5a0139f
Alexey Pyltsyn <lex61rus@gmail.com>
2020-02-03 19:00:27 +0300

    Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React from 'react';
 import classnames from 'classnames';
 import Layout from '@theme/Layout';
@@ -17,85 +10,150 @@ const features = [
   {
     title: <>Easy to Use</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    imagePosition: 'left',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Docusaurus was designed from the ground up to be easily installed and used to get your website up and running
+        quickly.
       </>
     ),
   },
   {
     title: <>Focus on What Matters</>,
     imageUrl: 'img/undraw_docusaurus_tree.svg',
+    imagePosition: 'right',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into the{' '}
+        <code>docs</code> directory.
       </>
     ),
   },
   {
     title: <>Powered by React</>,
     imageUrl: 'img/undraw_docusaurus_react.svg',
+    imagePosition: 'left',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the same
+        header and footer.
+      </>
+    ),
+  },
+  {
+    title: <>Focus on What Matters</>,
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    imagePosition: 'right',
+    description: (
+      <>
+        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into the{' '}
+        <code>docs</code> directory.
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imagePosition, imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
+
   return (
-    <div className={classnames('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
+    <div className="row">
+      <div
+        className={classnames('col col--5', {
+          [styles.featureImageSecond]: imagePosition === 'right',
+          'col--offset-2': imagePosition === 'right',
+        })}>
+        <div className={styles.featureImage}>
+          <img src={imgUrl} alt={title} />
         </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
+      </div>
+
+      <div
+        className={classnames('col col--5 padding-vert--xl', {
+          'col--offset-2': imagePosition === 'left',
+        })}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
     </div>
   );
 }
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
+  const logo = useBaseUrl(siteConfig.themeConfig.navbar.logo.src);
+
   return (
     <Layout
       title={`${siteConfig.title} - The ubiquitous test and mock framework for PowerShell`}
       description="Description will go into a meta tag in <head />">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/quick-start')}>
-              Get Started
-            </Link>
+          <div className="row">
+            <div className="col col--9">
+              <h1 className="hero__title">{siteConfig.title}</h1>
+              <p className="hero__subtitle">{siteConfig.tagline}</p>
+
+              <div className={styles.buttons}>
+                <Link
+                  className={classnames('button button--secondary button--lg', styles.getStarted)}
+                  to={useBaseUrl('docs/quick-start')}>
+                  Get Started
+                </Link>
+              </div>
+            </div>
+
+            <div className="col text--center">
+              <img alt="" src={logo} width="55%" />
+            </div>
           </div>
         </div>
       </header>
+
       <main>
-        {features && features.length && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
+        <div className="container">
+          {features && features.length && (
+            <section className={styles.features}>
+              <div className="container">
                 {features.map((props, idx) => (
                   <Feature key={idx} {...props} />
                 ))}
               </div>
+            </section>
+          )}
+
+          <section className={classnames(styles.openCollective, 'padding-vert--lg padding-horiz--md')}>
+            <h2>Support us on Open Collective!</h2>
+
+            <p>
+              Open Collective is an initiative that allows community members an easy and transparent way to donate to
+              open source projects. <br />
+            </p>
+
+            <div className="backers-section">
+              <h3>Individuals</h3>
+
+              <a href="https://opencollective.com/pester#backers" target="_blank">
+                <img src="https://opencollective.com/pester/backers.svg?width=695" />
+              </a>
             </div>
+
+            <div className="backers-section">
+              <h3>Organizations</h3>
+
+              <a href="https://opencollective.com/pester#sponsors" target="_blank">
+                <img src="https://opencollective.com/pester/sponsors.svg" />
+              </a>
+            </div>
+
+            <br />
+
+            <a href="https://opencollective.com/pester/donate" target="_blank" rel="nofollow noopener noreferrer">
+              <img alt="open-collective" src="https://opencollective.com/pester/contribute/button.png?color=blue" />
+            </a>
           </section>
-        )}
+        </div>
       </main>
     </Layout>
   );
