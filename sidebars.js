7c21deaf766be7d8493cdc515938e9b02dc02a3e
nohwnd <me@jakubjares.com>
2020-11-08 21:54:45 +0100

    Write multiple docs

--- sidebars.js
+++ sidebars.js
@@ -27,9 +27,16 @@ module.exports = {
     Introduction: [
       "quick-start",
       "introduction/installation",
-      "introduction/powershell-core-limitations",
     ],
     Usage: [
+      "usage/file-placement-and-naming",
+      "usage/importing-tested-functions",
+      "usage/test-file-structure",
+      "usage/discovery-and-run",
+      "usage/setup-and-teardown",
+      "usage/test-cases",
+      // "usage/generating-tests",
+      // "usage/script-parameters",
       "usage/assertions",
       "usage/mocking",
       "usage/modules",
