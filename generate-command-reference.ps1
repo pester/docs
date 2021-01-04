8c51b6987d356c4ab8dd1fdae712e99766744ae7
Glenn Sarti <glennsarti@users.noreply.github.com>
2021-01-04 09:42:01 +0800

    (GH-84) Refactor module version detection and installation
    Previously the module version detection could not be performed on other
    dependant modules and copied code unnecessarily.  This commit refactors the
    command reference script to be able to specify all module versions (default to
    latest) and removes duplicate code.

--- generate-command-reference.ps1
+++ generate-command-reference.ps1
@@ -14,8 +14,13 @@
   .LINK
     https://docusaurus-powershell.netlify.app/docs/faq/ci-cd
 #>
+[CmdletBinding()]
 param (
-  [Parameter(Mandatory = $False)][string] $PesterVersion
+  [Parameter(Mandatory = $False)][string] $PesterVersion,
+
+  [Parameter(Mandatory = $False)][string] $PlatyPSVersion,
+
+  [Parameter(Mandatory = $False)][string] $DocusaurusVersion
 )
 Set-StrictMode -Version Latest
 $PSDefaultParameterValues['*:ErrorAction'] = "Stop"
@@ -23,49 +28,40 @@ $PSDefaultParameterValues['*:ErrorAction'] = "Stop"
 Write-Host "Generating MDX files for website Command Reference" -BackgroundColor DarkGreen
 
 # -----------------------------------------------------------------------------
-# Fetch module versions from PSGallery
-# -----------------------------------------------------------------------------
-$modules = @{ }
-
-Write-Host "Fetching module versions from PSGallery..."
-if ($PesterVersion) {
-  $modules.Pester = $PesterVersion
-}
-else {
-  $modules.Pester = (Find-Module -Name Pester).Version
-}
-
-$modules."Alt3.Docusaurus.Powershell" = (Find-Module -Name Alt3.Docusaurus.Powershell).Version
-$modules.PlatyPS = (Find-Module -Name PlatyPS).Version
-
-# -----------------------------------------------------------------------------
 # Install required modules
 # -----------------------------------------------------------------------------
-$modules.GetEnumerator() | ForEach-Object {
-  Write-Host "Requires $($_.Name) $($_.Value)"
+$ModuleList = [ordered]@{
+  'PlatyPS' = $PlatyPSVersion
+  'Alt3.Docusaurus.Powershell' = $DocusaurusVersion
+  'Pester' = $PesterVersion
+}
+# Can't use the original enumerator here because we may modify the dictionary mid-process
+$ModuleList.Keys.Clone() | ForEach-Object {
+  $ModuleName = $_
+  $RequestedVersion = $ModuleList.Item($ModuleName)
+  Write-Host "Requires $ModuleName $RequestedVersion"
 
-  $installed = Get-Module -ListAvailable $_.Name
-
-  if (-not $installed) {
-    Write-Host "=> no versions installed: installing $($_.Value)"
-    Install-Module $_.Name -RequiredVersion $_.Value -Force -SkipPublisherCheck -AllowClobber -Scope CurrentUser
-    Write-Host "=> importing"
-    Import-Module -Name $_.Name -RequiredVersion $_.Value -Force
-
-    return
+  if ([String]::IsNullOrEmpty($RequestedVersion)) {
+    Write-Host "=> Fetching module versions of $ModuleName from PSGallery..."
+    $RequestedVersion = (Find-Module -Name $ModuleName).Version
+    $ModuleList.Item($ModuleName) = $RequestedVersion
+    Write-Host "=> PSGallery version is $RequestedVersion"
   }
 
-  if ($installed.Version -contains $_.Value) {
+  $Installed = Get-Module -ListAvailable $ModuleName
+  if ($Installed -and ($Installed.Version -contains $RequestedVersion)) {
     Write-Host "=> required version already installed"
-    Import-Module -Name $_.Name -RequiredVersion $_.Value -Force
-
-    return
+  } else {
+    if (-not $Installed) {
+      Write-Host "=> no versions installed: installing $RequestedVersion"
+    } else {
+      Write-Host "=> no matching version installed: installing $RequestedVersion"
+    }
+    Install-Module $ModuleName -RequiredVersion $RequestedVersion -Force -SkipPublisherCheck -AllowClobber -Scope CurrentUser
   }
 
-  Write-Host "=> no matching version installed: installing $($_.Value)"
-  Install-Module $_.Name -RequiredVersion $_.Value -Force -SkipPublisherCheck -AllowClobber -Scope CurrentUser
   Write-Host "=> importing"
-  Import-Module -Name $_.Name -RequiredVersion $_.Value -Force
+  Import-Module -Name $ModuleName -RequiredVersion $RequestedVersion -Force
 }
 
 # -----------------------------------------------------------------------------
@@ -89,7 +85,7 @@ $docusaurusOptions = @{
     "Help"
     "Documentation"
   )
-  AppendMarkdown  = "## EDIT THIS PAGE`nThis page was auto-generated using the comment based help in Pester $($modules.Pester). To edit the content of this page, change the corresponding help in the [pester/Pester](https://github.com/pester/pester) repository. See our [contribution guide](https://github.com/pester/docs#contributing) for more information."
+  AppendMarkdown  = "## EDIT THIS PAGE`nThis page was auto-generated using the comment based help in Pester $($ModuleList.Pester). To edit the content of this page, change the corresponding help in the [pester/Pester](https://github.com/pester/pester) repository. See our [contribution guide](https://github.com/pester/docs#contributing) for more information."
 }
 
 # -----------------------------------------------------------------------------
