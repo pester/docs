d6f2c4fc34c23d49699a668a23e61080035fe5a4
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2022-05-10 19:14:33 +0000

    Revert "add Discord community link"
    This reverts commit 437fc2a2b63e304ca2d67231451a9815a52313e0.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,8 @@ module.exports = {
           items: [
             {
               label: 'Gitter',
-              href: 'https://gitter.im/pester/Pester',
+              href:
+                'https://gitter.im/pester/Pester',
             },
             {
               label: 'PowerShell.org',
@@ -73,10 +74,6 @@ module.exports = {
             {
               label: 'Slack',
               href: 'https://powershell.slack.com/messages/C03QKTUCS',
-            },
-            {
-              label: 'Discord (#testing)',
-              href: 'https://discord.gg/powershell',
             },
             {
               label: 'Stack Overflow',
