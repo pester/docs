56a963cbf3fd5ca0752d9095c9060c8fabbb3b20
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2023-05-03 18:30:54 +0000

    Add Giscus to docs-pages

---
+++ src/components/Giscus.js
@@ -0,0 +1,50 @@
+import Head from '@docusaurus/Head';
+import { useLocation } from '@docusaurus/router';
+import { useColorMode } from '@docusaurus/theme-common';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Giscus from "@giscus/react";
+import React from 'react';
+
+/**
+ * Adds a meta-tag to ensure new dicussion always add the canonical link in the initial post.
+ * Avoids any localhost etc. if first reaction/post was made from a preview build
+ * 
+ * See https://github.com/giscus/giscus/blob/main/ADVANCED-USAGE.md#giscusbacklink
+ */
+export function GiscusHead() {
+    const {
+        siteConfig: { url: siteUrl },
+    } = useDocusaurusContext();
+    const { pathname } = useLocation();
+    const canonicalUrl = siteUrl + useBaseUrl(pathname);
+
+    return (
+        <Head>
+            <meta name="giscus:backlink" content={canonicalUrl} />
+        </Head>
+    )
+}
+
+export function GiscusComponent() {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Giscus
+      repo="fflaten/docs"
+      repoId="MDEwOlJlcG9zaXRvcnkzMDY2ODYxNTc"
+      category="Comments"
+      categoryId="DIC_kwDOEkeozc4CWO6M"
+      mapping="pathname"
+      strict="1"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme={colorMode}
+      lang="en"
+      loading="lazy"
+      crossorigin="anonymous"
+      async
+    />
+  );
+}
