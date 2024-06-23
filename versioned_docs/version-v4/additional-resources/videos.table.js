c37a1f31a3d476f8e16bbba10ae87ec452c4d98f
Ronald Borman <ronald.borman@gmail.com>
2024-06-23 22:38:10 +0200

    Updating versioned docs

--- versioned_docs/version-v4/additional-resources/videos.table.js
+++ versioned_docs/version-v4/additional-resources/videos.table.js
@@ -9,36 +9,42 @@ export const videos = [
     author: "Mark Wragg",
     date: "2018-10-10",
     url: "https://www.youtube.com/watch?v=BbOiQCgDDR8",
+    event: "PSDay.UK 2018",
   },
   {
     title: "Introduction to testing with Pester",
-    author: "Jakub Jares",
+    author: "Jakub Jareš",
     date: "2017-04-08",
     url: "https://www.youtube.com/watch?v=F3oOk0BC9B4",
+    event: "PowerShell UG Singapore",
   },
   {
     title: "Testing PowerShell with Pester",
     author: "Robert Cain",
     date: "2016-06-18",
     url: "https://www.pluralsight.com/courses/powershell-testing-pester",
+    event: "",
   },
   {
     title: "Pester the Tester PowerShell Bugs Beware",
     author: "Robert Cain",
     date: "2016-06-14",
     url: "https://www.youtube.com/watch?v=o4ihc7atwYQ",
+    event: "Mississippi PowerShell User Group",
   },
   {
     title: "Test-Driven Development with Pester",
     author: "June Blender",
-    date: "2016-05-21",
+    date: "2016-04-21",
     url: "https://www.youtube.com/watch?v=gssAtCeMOoo",
+    event: "PowerShell Conference EU 2016",
   },
   {
-    title: "Pester in  Action - series",
+    title: "Pester in Action - series",
     author: "Kevin Marquett",
     date: "2015-11-11",
     url: "https://www.youtube.com/playlist?list=PLOcTmsj9WHDo2_FfKePLaq_mJTcnW_fEJ",
+    event: "",
   },
 ];
 
@@ -65,5 +71,10 @@ export const columns = [
     header: "Date",
     accessorKey: "date",
     className: "pester-data-table nowrap",
+  },
+  {
+    header: "Event",
+    accessorKey: "event",
+    className: "pester-data-table left",
   },
 ];
