b70ce8bda0695089060b3200cf40e869ea733afa
Ronald Borman <ronald.borman@gmail.com>
2024-06-23 22:55:09 +0200

    Removing videos

--- docs/additional-resources/videos.table.js
+++ docs/additional-resources/videos.table.js
@@ -61,13 +61,6 @@ export const videos = [
     event: "PowerShell Conference EU 2019",
   },
   {
-    title: "Pester + Azure (Monitor + Automation)",
-    author: "Mateusz Czerniawski",
-    date: "2019-06-05",
-    url: "https://www.youtube.com/watch?v=CMul62ef214",
-    event: "PowerShell Conference EU 2019",
-  },
-  {
     title: "Breakout Session: Community Talk about Pester 5",
     author: "Jakub Jareš",
     date: "2019-06-05",
@@ -89,53 +82,11 @@ export const videos = [
     event: "PowerShell Conference EU 2019",
   },
   {
-    title: "Mastering PowerShell testing with Pester",
-    author: "Mark Wragg",
-    date: "2018-10-10",
-    url: "https://www.youtube.com/watch?v=BbOiQCgDDR8",
-    event: "PSDay.UK 2018",
-  },
-  {
     title: "Common mistakes in Pester tests",
     author: "Jakub Jareš",
     date: "2018-04-20",
     url: "https://www.youtube.com/watch?v=VBSxCXmNqQ8",
     event: "PowerShell Conference EU 2018",
-  },
-  {
-    title: "Introduction to testing with Pester",
-    author: "Jakub Jareš",
-    date: "2017-04-08",
-    url: "https://www.youtube.com/watch?v=F3oOk0BC9B4",
-    event: "PowerShell UG Singapore",
-  },
-  {
-    title: "Testing PowerShell with Pester",
-    author: "Robert Cain",
-    date: "2016-06-18",
-    url: "https://www.pluralsight.com/courses/powershell-testing-pester",
-    event: "",
-  },
-  {
-    title: "Pester the Tester PowerShell Bugs Beware",
-    author: "Robert Cain",
-    date: "2016-06-14",
-    url: "https://www.youtube.com/watch?v=o4ihc7atwYQ",
-    event: "Mississippi PowerShell User Group",
-  },
-  {
-    title: "Test-Driven Development with Pester",
-    author: "June Blender",
-    date: "2016-04-21",
-    url: "https://www.youtube.com/watch?v=gssAtCeMOoo",
-    event: "PowerShell Conference EU 2016",
-  },
-  {
-    title: "Pester in Action - series",
-    author: "Kevin Marquett",
-    date: "2015-11-11",
-    url: "https://www.youtube.com/playlist?list=PLOcTmsj9WHDo2_FfKePLaq_mJTcnW_fEJ",
-    event: "",
   },
 ];
 
